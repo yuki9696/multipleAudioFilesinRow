f0542124ab532666158dd2389911d07364f06d78
yuki9696 <yukiyamamoto@Yuki-no-MacBook-Air.local>
2017-02-17 20:55:12 +0900

    deleted comments

--- audioDidFinish/AppDelegate.swift
+++ audioDidFinish/AppDelegate.swift
@@ -49,7 +49,6 @@ ional since there are legitimate
         let container = NSPersistentContainer(name: "audioDidFinish")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                  
                 /*
